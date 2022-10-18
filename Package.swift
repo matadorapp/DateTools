c62c00ef5c32eaf06cd9a4362466045fe77e8797
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-10-18 13:35:19 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    products: [
+        .library(name: "DateToolsSwift", targets: ["DateToolsSwift"])
+    ],
     targets: [
         .target(name: "DateToolsSwift", path: "DateToolsSwift/DateTools")
     ]
